2bd7b73bc91e71de10916fec1ca65935c3d2c3f6
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-08 22:12:31 -0300

    criando o model de Patient_surgeries

---
+++ models/PatientSurgery.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PatientSurgery = sequelize.define('PatientSurgery', {}, { timestamps: false });
+
+  PatientSurgery.associate = (models) => {
+    models.Patient.belongsToMany(models.Surgery, {
+      as: 'surgeries',
+      through: PatientSurgery,
+      foreignKey: 'patient_id',
+      otherKey: 'surgery_id',
+    });
+    models.Surgery.belongsToMany(models.Patient, {
+      as: 'patients',
+      through: PatientSurgery,
+      foreignKey: 'surgery_id',
+      otherKey: 'patient_id',
+    });
+  };
+
+  return PatientSurgery;
+};
